4a865866fefd87a6c056863717bc6b18e532ddaf
DuyHoang11022004 <nguyenduyhoangvta1607@gmail.com>
2024-10-18 19:45:40 +0700

    SQLDAO

---
+++ MySQLDAO/src/hoang/dev/data/dao/CategoryDao.java
@@ -0,0 +1,15 @@
+package hoang.dev.data.dao;
+
+import java.util.List;
+
+import hoang.dev.data.model.Category;
+
+public interface CategoryDao {
+	public boolean insert(Category category);
+	public boolean update(Category category);
+	public boolean delete(int id );
+	public Category find(int id );
+	
+	public List<Category> findAll();
+
+}
---
+++ MySQLDAO/src/hoang/dev/util/Constants.java
@@ -0,0 +1,8 @@
+package hoang.dev.util;
+
+public class Constants {
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/shop";
+	public static final String DB_USER = "root";
+
+	public static final String DB_PASS = "";
+}
